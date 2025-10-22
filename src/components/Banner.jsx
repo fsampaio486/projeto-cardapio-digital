d5d96c0dbafc628f8f063deb3bd7cbc693ced061
Franciele Sampaio <f.sampaio21@outlook.com>
2025-10-22 12:33:59 -0300

    atualização

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,11 +1,11 @@
 import imgBanner from "../assets/imagens/hashtaurante.webp";
 
 const Banner = () => {
-    return (
-        <div className="banner">
-            <img src={imgBanner} alt="" />
-        </div>
-    )
-}
+  return (
+    <div className="banner">
+      <img src={imgBanner} alt="" />
+    </div>
+  );
+};
 
 export default Banner;
